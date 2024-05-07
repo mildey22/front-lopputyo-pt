85abff15b69fdad6bab32d24c746f471418dc6b8
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-05-07 19:52:35 +0300

    Added favicon

--- src/components/AddTraining.jsx
+++ src/components/AddTraining.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { fetchCustomers } from '../customerapi';
 
 export default function AddTraining({ handleAdd }) {
     const [open, setOpen] = useState(false);
     const [training, setTraining] = useState({
-        date: '',
+        date: null,
         activity: '',
         duration: '',
         customer: ''
     })
+
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        handleFetch();
+    }, []);
+
+    const handleFetch = () => {
+        fetchCustomers()
+            .then(data => setCustomers(data._embedded.customers))
+            .catch(err => console.error(err))
+    }
+
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -38,54 +59,50 @@ export default function AddTraining({ handleAdd }) {
             >
                 <DialogTitle>New training</DialogTitle>
                 <DialogContent>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="Date"
+                            value={training.date}
+                            //After using the date picker, the site crashes. I have a feeling it has to do with using it using dayjs.
+                            //Maybe it clashes with date-fns? My skills end here unfortunately
+                            onChange={date => setTraining({ ...training, date: date.toISOString() })}
+                            renderInput={(params) => <TextField {...params} />}
+                            fullWidth
+                            variant="standard"
+                        />
+                    </LocalizationProvider>
                     <TextField
                         margin="dense"
-                        label="Brand"
-                        value={training.date}
-                        onChange={e => setTraining({...training, date: e.target.value})}
+                        label="Activity"
+                        value={training.activity}
+                        onChange={e => setTraining({ ...training, activity: e.target.value })}
                         fullWidth
                         variant="standard"
                     />
                     <TextField
                         margin="dense"
-                        label="Model"
-                        value={car.model}
-                        onChange={e => setCar({...car, model: e.target.value})}
+                        label="Duration (min)"
+                        value={training.duration}
+                        onChange={e => setTraining({ ...training, duration: e.target.value })}
                         fullWidth
                         variant="standard"
                     />
-                    <TextField
-                        margin="dense"
-                        label="Color"
-                        value={car.color}
-                        onChange={e => setCar({...car, color: e.target.value})}
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        margin="dense"
-                        label="Fuel"
-                        value={car.fuel}
-                        onChange={e => setCar({...car, fuel: e.target.value})}
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        margin="dense"
-                        label="Model Year"
-                        value={car.modelYear}
-                        onChange={e => setCar({...car, modelYear: e.target.value})}
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        margin="dense"
-                        label="Price (€)"
-                        value={car.price}
-                        onChange={e => setCar({...car, price: e.target.value})}
-                        fullWidth
-                        variant="standard"
-                    />
+                    <FormControl fullWidth>
+                        <InputLabel id="customer">Customer</InputLabel>
+                        <Select
+                            labelId="customer"
+                            id="customer"
+                            value={training.customer}
+                            label="Customer"
+                            onChange={e => setTraining({ ...training, customer: e.target.value })}
+                        >
+                            {customers.map(customer => (
+                                <MenuItem key={customer.id} value={customer}>
+                                    {customer.firstname} {customer.lastname}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
--- src/components/Trainings.jsx
+++ src/components/Trainings.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import { AgGridReact } from 'ag-grid-react';
 import { parseISO } from "date-fns";
 import format from "date-fns/format";
-//import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import { fetchTrainings } from "../trainingapi";
-//import AddTraining from "./AddTraining";
-//import EditTraining from "./EditTraining";
+import AddTraining from "./AddTraining";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
@@ -26,6 +25,13 @@ function Trainings() {
         valueGetter: (p) =>
         p.data.customer.firstname + " " + p.data.customer.lastname 
         },
+        //I'm not sure how to get ID from the gettrainings endpoint because it doesn't have a href link
+        {
+            cellRenderer: params =>
+                <Button size="small" color="error" onClick={() => handleDelete(params.data.id)}>
+                    Delete
+                </Button>, width: 120
+        }
     ]);
 
     const handleFetch = () => {
@@ -34,12 +40,44 @@ function Trainings() {
             .catch(err => console.error(err))
     }
 
+    const handleAdd = (newTraining) => {
+        fetch(import.meta.env.VITE_API_TRAININGS, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(newTraining)
+        })
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("Error in adding a training: " + response.statusText)
+
+                return response.json();
+            })
+            .then(() => handleFetch())
+            .catch(err => console.error(err))
+    }
+
+    //Can't unfortunately get delete function to work here 
+    const handleDelete = (url) => {
+        if (window.confirm("Are you sure?")) {
+            fetch(url, { method: 'DELETE' })
+                .then(response => {
+                    if (!response.ok)
+                        throw new Error("Error in deleting training: " + response.statustext);
+
+                    return response.json();
+                })
+                .then(() => setTrainings())
+                .catch(err => console.error(err))
+        }
+    }
+
     function formatDate(params) {
         return format(parseISO(params.value), "dd-MM-yyy HH:mm");
       }
 
     return (
         <>
+        <AddTraining handleAdd={handleAdd} />
             <div className="ag-theme-material" style={{ height: 600 }}>
                 <AgGridReact
                     rowData={trainings}
